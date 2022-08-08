576b4c60373446945dc8e5607dc58b43b979a3fa
Solace <101289242+Solalace@users.noreply.github.com>
2022-08-08 20:07:19 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -5,3 +5,13 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
 
 export XDG_CONFIG_DIRS="/etc/xdg"
+
+export XDG_DOWNLOAD_DIR="$HOME/Downloads"
+export XDG_DOCUMENTS_DIR="$HOME/Documents"
+export XDG_PICTURES_DIR="$HOME/Pictures"
+export XDG_VIDEOS_DIR="$HOME/Videos"
+
+# export BROWSER="qutebrowser"
+# export EDITOR="nvim"
+
+# source "$HOME/.cargo/env"
