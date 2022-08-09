52cada3d8d9e3ed8deeb6d14ff31932efae59075
Solace <101289242+Solalace@users.noreply.github.com>
2022-08-09 01:59:14 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -11,7 +11,14 @@ export XDG_DOCUMENTS_DIR="$HOME/Documents"
 export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_VIDEOS_DIR="$HOME/Videos"
 
+export XAUTHORITY="$XDG_CONFIG_HOME/_main/X11/Xauthority"
+export XINITRC="$XDG_CONFIG_HOME/_main/X11/xinitrc"
+
 # export BROWSER="qutebrowser"
 # export EDITOR="nvim"
 
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/pass"
+
+# export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+# export CARGO_HOME="$XDG_DATA_HOME/cargo"
 # source "$HOME/.cargo/env"
