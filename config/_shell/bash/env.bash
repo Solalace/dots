978200a87f465b77bcff991cf84654c0db29a874
Solace <101289242+Solalace@users.noreply.github.com>
2022-08-23 17:04:15 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -12,10 +12,17 @@ export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_VIDEOS_DIR="$HOME/Videos"
 
 export XAUTHORITY="$XDG_CONFIG_HOME/_main/X11/Xauthority"
+export XRESOURCES="$XDG_CONFIG_HOME/_main/X11/Xresources"
 export XINITRC="$XDG_CONFIG_HOME/_main/X11/xinitrc"
 
-# export BROWSER="qutebrowser"
-# export EDITOR="nvim"
+export KITTY_CONFIG_DIRECTORY="$XDG_CONFIG_HOME/_term/kitty"
+
+export WINDOW_MANAGER="bspwm"
+export BROWSER="qutebrowser"
+#xport TERMINAL="kitty"
+export VISUAL="nvim"
+export EDITOR="nvim"
+export SHELL="/usr/bin/bash"
 
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME/pass"
 
