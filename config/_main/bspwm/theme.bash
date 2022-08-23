3b4bc2f92e0adac3831681f41066fdd5f5daef54
Solace <101289242+Solalace@users.noreply.github.com>
2022-08-23 16:37:02 +0000

    Create theme.bash

---
+++ config/_main/bspwm/theme.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+foreground='#20252b'
+background='#1c2026'
+black='#101317'
+red='#f87070'
+green='#79dcaa'
+yellow='#ffe59e'
+blue='#7ab0df'
+magenta='#b77ee0'
+cyan='#54ced6'
+white='#d4d4d5'
+grey='#171c21'
+shade='#1c2126'
