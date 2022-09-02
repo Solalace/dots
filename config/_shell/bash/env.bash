847a01748ab410276209243d72370c2053848b1c
Solace <101289242+Solalace@users.noreply.github.com>
2022-09-02 17:59:43 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -2,9 +2,12 @@
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_STATE_HOME="$HOME/.var"
 export XDG_DATA_HOME="$HOME/.local/share"
 
 export XDG_CONFIG_DIRS="/etc/xdg"
+
+export XDG_STATE_HOME"/bash/history"
 
 export XDG_DOWNLOAD_DIR="$HOME/Downloads"
 export XDG_DOCUMENTS_DIR="$HOME/Documents"
@@ -14,6 +17,8 @@ export XDG_VIDEOS_DIR="$HOME/Videos"
 export XAUTHORITY="$XDG_CONFIG_HOME/_main/X11/Xauthority"
 export XRESOURCES="$XDG_CONFIG_HOME/_main/X11/Xresources"
 export XINITRC="$XDG_CONFIG_HOME/_main/X11/xinitrc"
+
+export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/_misc/npm/npmrc
 
 export KITTY_CONFIG_DIRECTORY="$XDG_CONFIG_HOME/_term/kitty"
 
