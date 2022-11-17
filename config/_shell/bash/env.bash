66a222193b2694287732ecf6787f1e706b9b32b7
Solace <101289242+Solalace@users.noreply.github.com>
2022-11-17 01:08:52 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -5,8 +5,8 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.var"
 export XDG_DATA_HOME="$HOME/.local/share"
 
+export PATH="${PATH}:/home/$USER/bin"
 export XDG_CONFIG_DIRS="/etc/xdg"
-
 export HISTFILE="$XDG_STATE_HOME/bash/history"
 
 export XDG_DESKTOP_DIR="$HOME/Desktop"
@@ -17,7 +17,6 @@ export XDG_VIDEOS_DIR="$HOME/Videos"
 
 export XAUTHORITY="$XDG_CONFIG_HOME/_main/X11/Xauthority"
 export XRESOURCES="$XDG_CONFIG_HOME/_main/X11/Xresources"
-export XINITRC="$XDG_CONFIG_HOME/_main/X11/xinitrc"
 
 export GTK_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-1.0/gtkrc"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-2.0/gtkrc"
