f8c2b84520cf08571ef32429dbc05e0264a43b89
Solace <101289242+Solalace@users.noreply.github.com>
2022-11-20 16:26:13 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export PATH="$PATH:${$(find ~/.local/bin -type d -printf %p:)%%:}"
+
 export SUDO_PROMPT="[$USER][sudo] password: "
 
 export XDG_CONFIG_HOME="$HOME/.config"
@@ -7,7 +9,6 @@ export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_STATE_HOME="$HOME/.var"
 export XDG_DATA_HOME="$HOME/.local/share"
 
-export PATH="${PATH}:/home/$USER/.bin"
 export XDG_CONFIG_DIRS="/etc/xdg"
 export HISTFILE="$XDG_STATE_HOME/bash/history"
 
@@ -17,10 +18,10 @@ export XDG_DOCUMENTS_DIR="$HOME/Documents"
 export XDG_PICTURES_DIR="$HOME/Pictures"
 export XDG_VIDEOS_DIR="$HOME/Videos"
 
-export GTK_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-1.0/gtkrc"
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-2.0/gtkrc"
 export GTK3_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-3.0/gtkrc"
 export GTK4_RC_FILES="$XDG_CONFIG_HOME/_ui/gtk-4.0/gtkrc"
+export QT_QPA_PLATFORMTHEME="gtk3"
 
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/_misc/npm/npmrc"
 
@@ -35,6 +36,7 @@ export EDITOR="nvim"
 export SHELL="/usr/bin/bash"
 export XDG_SESSION_TYPE="x11"
 
+export WINEPREFIX="$XDG_DATA_HOME/wineprefixes/default"
 export PASSWORD_STORE_DIR="$XDG_DATA_HOME/pass"
 
 # export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
