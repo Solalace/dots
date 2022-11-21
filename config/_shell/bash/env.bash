250b4dfe6b5e6fc86c0a7f0cf2bb3152c9a7fc06
Solace <101289242+Solalace@users.noreply.github.com>
2022-11-21 00:01:58 +0000

    Update env.bash

--- config/_shell/bash/env.bash
+++ config/_shell/bash/env.bash
@@ -26,7 +26,6 @@ export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/_misc/npm/npmrc"
 
 export KITTY_CONFIG_DIRECTORY="$XDG_CONFIG_HOME/_term/kitty"
 
-# export QT_QPA_PLATFORMTHEME="gtk3"
 export QT_QPA_PLATFORMTHEME="qt5ct"
 export WINDOW_MANAGER="bspwm"
 export BROWSER="qutebrowser"
