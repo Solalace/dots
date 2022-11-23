57c68eca8ce92053e8978df800afce6754a5b89e
Solace <101289242+Solalace@users.noreply.github.com>
2022-11-23 10:27:44 +0000

    Update alias.bash

--- config/_shell/bash/alias.bash
+++ config/_shell/bash/alias.bash
@@ -1,1 +1,30 @@
 #!/usr/bin/env bash
+
+alias lf="ls -f"
+alias la="ls -la"
+alias ll="ls -ll"
+
+alias cd..='cd ../'
+alias cd...='cd ../../'
+alias cd....='cd ../../../'
+alias cd.....='cd ../../../../'
+alias cd......='cd ../../../../../'
+alias ..='cd ../'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+alias nv="nvim"
+alias nano="nvim"
+
+alias cls="clear"
+
+alias grepin='grep -i'
+
+alias _='sudo'
+
+alias wget=wget --hsts-file="$XDG_DATA_HOME/wget-hsts
+
+alias logout="bspc quit"
+alias logoff="bspc quit"
