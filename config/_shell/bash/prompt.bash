bc635e0a7a8f51de02f6bbcc4612361c5db6be8a
Solace <101289242+Solalace@users.noreply.github.com>
2023-01-03 22:53:25 -0500

    Update prompt.bash

--- config/_shell/bash/prompt.bash
+++ config/_shell/bash/prompt.bash
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-_Bold="$(tput bold)"
-_RESET="\e[0m"
+stty -ixon # Disable ctrl-s and ctrl-q
 
-PS1='[\u@\h \W]\$ '
-PS2=""
-PS3=""
+# Setting Bash Prompt
+if [ "$EUID" -ne 0 ];
+then 
+  export PS1="[\[\e]0;\u@\h \w\a\]\[$(tput bold)\]\[\033[38;5;218m\]\u@\[\033[38;5;60m\]\h\[\033[00m\] \[\033[38;5;183m\]\w\[\033[00m\]]$ "
+else 
+  export PS1="[\[\e]0;\u@\h \w\a\]\[$(tput bold)\]\[\033[38;5;218m\]\u@\[\033[38;5;60m\]\h\[\033[00m\] \[\033[38;5;183m\]\w\[\033[00m\]]# "
+fi
+
+export GPG_TTY=$(tty)
+
+PS2="\[❯\] "
