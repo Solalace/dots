10022c299d97b98b08694b632689c8012a38cec5
Solace <101289242+Solalace@users.noreply.github.com>
2023-01-05 22:29:24 -0500

    Update settings.lua

--- config/_term/nvim/lua/settings.lua
+++ config/_term/nvim/lua/settings.lua
@@ -1,1 +1,22 @@
 
+-- Variables
+local o = vim.o
+
+-- Colors
+o.termguicolors = false
+
+-- Editing
+o.number = true
+o.relativenumber = true
+o.autoindent = false
+o.shiftwidth = 2
+o.softtabstop = 2
+o.tabstop = 2
+--o.listchars = ''
+
+-- Clipboard
+
+-- History
+o.history = 80
+
+-- Undo and Backup
