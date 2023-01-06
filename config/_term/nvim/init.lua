acceb8166d16283bcab2673e289d71e722ce35f1
Solace <101289242+Solalace@users.noreply.github.com>
2023-01-06 03:26:13 +0000

    Update and rename vimrc to init.lua

---
+++ config/_term/nvim/init.lua
@@ -0,0 +1,6 @@
+vim.o.directory   ="$XDG_CACHE_HOME/vim,/tmp"
+vim.o.backupdir   ="$XDG_CACHE_HOME/vim,/tmp"
+vim.o.viminfofile ="$XDG_CACHE_HOME/vim/viminfo"
+vim.o.runtimepath ="$XDG_TERM_CONFIG/nvim,$VIM,$VIMRUNTIME"
+
+require('settings')
