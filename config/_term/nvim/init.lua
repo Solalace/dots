13aab83bd01de008a42a8fc87056b9891f809c41
Solace <101289242+Solalace@users.noreply.github.com>
2023-01-19 16:51:41 +0000

    Update init.lua

--- config/_term/nvim/init.lua
+++ config/_term/nvim/init.lua
@@ -1,6 +1,5 @@
 vim.o.directory   = "$XDG_CACHE_HOME/vim,/tmp"
 vim.o.backupdir   = "$XDG_CACHE_HOME/vim,/tmp"
-vim.o.viminfofile = "$XDG_CACHE_HOME/vim/viminfo"
 vim.o.runtimepath = "$XDG_TERM_CONFIG/nvim,/usr/share/nvim/runtime"
 
 require('settings')
