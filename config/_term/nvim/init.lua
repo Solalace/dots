5aa4e23988722024488c69547edaf6cca080260e
Solace <101289242+Solalace@users.noreply.github.com>
2023-01-19 16:34:06 +0000

    Update init.lua

--- config/_term/nvim/init.lua
+++ config/_term/nvim/init.lua
@@ -1,14 +1,7 @@
 vim.o.directory   = "$XDG_CACHE_HOME/vim,/tmp"
 vim.o.backupdir   = "$XDG_CACHE_HOME/vim,/tmp"
 vim.o.viminfofile = "$XDG_CACHE_HOME/vim/viminfo"
-vim.o.runtimepath = "$XDG_TERM_CONFIG/nvim,$VIM,$VIMRUNTIME"
+vim.o.runtimepath = "$XDG_TERM_CONFIG/nvim,/usr/share/nvim/runtime"
 
 require('settings')
-
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
-end)
+require('plugins')
