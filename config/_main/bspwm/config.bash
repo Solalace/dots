092e036e8ba85a92fa158b2a778e2a6e19a53483
Solace <101289242+Solalace@users.noreply.github.com>
2023-02-02 13:54:34 -0500

    Update config.bash

--- config/_main/bspwm/config.bash
+++ config/_main/bspwm/config.bash
@@ -6,8 +6,3 @@ export XDG_CURRENT_DESKTOP="bspwm"
 source "$XDG_MAIN_CONFIG/bspwm/rules.bash"
 source "$XDG_MAIN_CONFIG/bspwm/default.bash"
 source "$XDG_MAIN_CONFIG/bspwm/launch.bash"
-
-# set root cursor
-xset b off
-xsetroot -cursor_name left_ptr
-xset mouse 0 0
