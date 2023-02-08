e0e82acbcac1b58beb27a03434ee4e2bdd87ba6e
Solace <101289242+Solalace@users.noreply.github.com>
2023-02-08 20:58:12 +0000

    Update prompt.bash

--- config/_shell/bash/prompt.bash
+++ config/_shell/bash/prompt.bash
@@ -3,7 +3,7 @@
 stty -ixon # Disable ctrl-s and ctrl-q
 
 # Setting Bash Prompt
-export PS1='\[\e[0m\][\[\e[0;1;38;5;218m\]\u\[\e[0;1;38;5;218m\]@\[\e[0;1;38;5;60m\]\h \[\e[0;38;5;183m\]\w\[\e[0m\]]\[\e[0m\]$ \[\e[0m\]'
+PS1='\[\e[0m\][\[\e[0;1;33m\]\u\[\e[0;1;33m\]@\[\e[0;1;38;5;242m\]\h \[\e[0;32m\]\w\[\e[0m\]]\[\e[0m\]$ \[\e[0m\]'
 
 export GPG_TTY=$(tty)
 
